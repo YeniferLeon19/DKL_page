1fd8bc26f5db50254fbc72049f1d3880e404da14
Yenifer Leon <yen.leon.martinez@gmail.com>
2026-06-25 16:32:47 -0500

    corrección de header y estilo

--- pagw - copia/js/header.js
+++ pagw - copia/js/header.js
@@ -1,37 +1,40 @@
 document.getElementById("header-container").innerHTML = `
 
-
-<header>
+<header class="main-header">
 
     <div class="logo">DarK Liquid</div>
 
     <nav class="nav-categorias">
-        <a href="../html/index.html" class="category-card">TIENDA</a>
-        <a href="../html/hombre.html" class="category-card">HOMBRE</a>
-        <a href="../html/mujer.html" class="category-card">MUJER</a>
+        <a href="../html/index.html">TIENDA</a>
+        <a href="../html/hombre.html">HOMBRE</a>
+        <a href="../html/mujer.html">MUJER</a>
     </nav>
 
-    <div class="search-container">
+    <div class="header-right">
 
-    <button class="search-btn" onclick="toggleSearch()">
-        
-    </button>
+        <div class="search-container">
 
-    <div class="search-bar" id="searchBar">
-        <input type="text" placeholder="Buscar productos...">
+            <button class="search-btn" onclick="toggleSearch()">
+                <img src="../componentes/lupa.png" class="search-icon">
+            </button>
+
+            <div class="search-bar" id="searchBar">
+                <input type="text" placeholder="Buscar productos...">
+            </div>
+
+        </div>
+
+        <a href="../html/carrito.html" class="cart-icon">
+            <img src="../componentes/carrito.png" class="cart-img">
+            <span class="cart-text">Carrito</span>
+            <span id="contador">0</span>
+        </a>
+
     </div>
-
-</div>
-
-
-<a href="../html/carrito.html" class="cart-icon">
-    <img src="../componentes/carrito.png" alt="Carrito" class="cart-img">
-
-    <span class="cart-text">Carrito</span>
-
-    <span id="contador">0</span>
-</a>
-
 
 </header>
 `;
+
+function toggleSearch() {
+    document.getElementById("searchBar").classList.toggle("active");
+}
